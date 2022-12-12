848cf66a5a7f34b8e8bacfe7c0fbc020d5b59732
DonataSni <45800141+DonataSni@users.noreply.github.com>
2022-12-13 00:19:11 +0200

    continued adding to compression

--- src/shannon/main.go
+++ src/shannon/main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"os"
 	"sort"
+	"math"
+	"strconv"
 
 	"github.com/icza/bitio"
 )
@@ -13,6 +15,7 @@ import (
 var in = flag.String("in", "/path/to/input", "input file")
 var out = flag.String("out", "/path/to/output", "output file")
 var l = flag.Int("l", 8, "word length is l")
+
 
 
 func main() {
@@ -29,6 +32,13 @@ func main() {
 	}
 	defer fi.Close()
 
+	fileStat, err := fi.Stat()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fileSize := uint64(fileStat.Size())
+
 	fo, err := os.Create(*out)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -36,32 +46,43 @@ func main() {
 	}
 	defer fo.Close()
 
-	compress(fi, fo, *l)
+	compress(fi, fo, *l, fileSize)
 }
+
+
 
 type pair struct {
 	word uint64
 	freq float64
 }
 
+type tuple struct {
+	word uint64
+	freq float64
+	len int
+}
+
 type byFreq []pair
 
-func compress(r io.Reader, w io.Writer, l int) {
+
+
+func compress(r io.Reader, w io.Writer, l int, fileSize uint64) {
 	bw := bitio.NewWriter(w)
 	defer bw.Close()
 	br := bitio.NewReader(r)
-	wordCount := 0
+	wordCount := fileSize / uint64(l)
+	leftover := fileSize % uint64(l)
 	var frequencies map[uint64]float64 = make(map[uint64]float64)
 
 
+
 	// reading and storing all words
-	for {
+	for i := 0; i < int(wordCount); i++ {
 		b, err := br.ReadBits(uint8(l))
 		if err != nil {
 			break
 		}
 
-		wordCount ++
 		_, ok := frequencies[b]
 		
 		if ok {
@@ -73,15 +94,64 @@ func compress(r io.Reader, w io.Writer, l int) {
 		b = 0
 	}
 
+	// saving the leftover tail
+	if leftover > 0 {
+		leftoverWord, err := br.ReadBits(uint8(l))
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+	}
+
+
 
 	var freqSorted []pair
+	var freqSummed []tuple
 
-	// saving and sorting frequencies
+	// saving, sorting and summing frequencies
 	for w, f := range frequencies {
 		frequencies[w] = f/float64(wordCount)
 		freqSorted = append(freqSorted, pair{word: w, freq: frequencies[w]})
 	}
+	
 	sort.Sort(byFreq(freqSorted))
+
+	freqSum := 0.0
+	for _, p := range freqSorted {
+		length := int(math.Ceil(-math.Log2(p.freq)))
+		freqSummed = append(freqSummed, tuple{word: p.word, freq: freqSum, len: length})
+		freqSum += p.freq
+	}
+
+
+	
+	// creating dictionary
+	var dictionary map[uint64]int64 = make(map[uint64]int64)
+
+	for _, t := range freqSummed {
+		code := ""
+		fraction := t.freq
+		for i := 0; i < t.len; i++ {
+			fraction *= 2
+			if fraction < 0 {
+				code += "0"
+			} else {
+				code += "1"
+				fraction -= 1
+			}
+		}
+
+		if c, err := strconv.ParseInt(code, 2, 64); err != nil {
+			fmt.Println(err)
+		} else {
+			dictionary[t.word] = c 
+		}
+	} 
+
+
+
+	// writing to file
+	// 
 }
 
 
